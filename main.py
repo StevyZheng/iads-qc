6ad0ec77d52edf93cbee544cb9b110ec405b0099
Stevy <664553716@qq.com>
2019-03-07 17:51:01 +0800

    init3

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from libs.sys import Sys
 from libs.util import TextOp
 
 
-print(TextOp.find_str("33  123456dfgr 234 \n  654dfw \n1234", ".*df.*", strip=True))
-print(TextOp.find_str_column("33  123456dfgr 234 \n  654dfw \n1234", ".*df.*", 1, " ", strip=True))
+print(Sys.get_cpu_info())
+
